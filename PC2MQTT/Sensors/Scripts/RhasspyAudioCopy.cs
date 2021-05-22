a41c2b7a2f80bbffb07b685dc515e367b96a4619
LostSoulfly <bradley@shaner.cloud>
2021-05-22 14:45:21 -0400

    Create RhasspyAudioCopy!

---
+++ PC2MQTT/Sensors/Scripts/RhasspyAudioCopy.cs
@@ -0,0 +1,44 @@
+﻿using BadLogger;
+using PC2MQTT.MQTT;
+using System;
+using System.Collections.Concurrent;
+
+namespace PC2MQTT.Sensors
+{
+    public partial class RhasspyAudioCopy : SensorBase, PC2MQTT.Sensors.ISensor
+    {
+
+        public new void ProcessMessage(MqttMessage mqttMessage)
+        {
+            var baseStation = "hassio";
+            var satellite = "satZero1";
+            var baseTopic = $"hermes/audioServer/{baseStation}/playBytes/";
+            var satTopic = $"hermes/audioServer/{satellite}/playBytes/";
+            var t = mqttMessage.GetRawTopic();
+            if (mqttMessage.GetRawTopic().Contains(baseTopic))
+            {
+                t = mqttMessage.GetRawTopic().Substring(baseTopic.Length);
+            } else { return; }
+
+            sensorHost.Publish(mqttMessage.SetTopic(satTopic).AddTopic(t));
+
+
+        }
+
+        public new void SensorMain()
+        {
+            base.SensorMain();
+
+            // Subscribe to ALL mqtt messages with a root multi-level wildcard
+            sensorHost.Subscribe(MqttMessageBuilder.
+                NewMessage().
+                SubscribeMessage.
+                AddTopic("hermes/audioServer/").
+                AddMultiLevelWildcard.
+                DoNotRetain.
+                QueueMessage.
+                Build());
+        }
+
+    }
+}
